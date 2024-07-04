c8d1549f513fc4ede3abc626cfc46d666d96864c
叶顺雨 <1481435885@qq.com>
2024-07-04 21:47:54 +0800

    Update database.h

---
+++ src/database/database.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <QCoreApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QDebug>
+#include <QString>
+#include <QVariantList>
+#include <QObject>
+QSqlDatabase db;//database
+class Database : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Database(QObject *parent = nullptr);
+    ~Database();
+
+    bool openDatabase(const QString &dbName);
+    void closeDatabase();
+
+    // operate users' information
+    bool addUser(const QString &username, const QString &password, double balance);
+    bool removeUser(int userId);
+    bool updateUser(int userId, const QString &username, const QString &password, double balance);
+    QVariantList getUser(int userId);
+
+    // operate stocks' information
+    bool addStock(const QString &symbol, const QString &name, double price);
+    bool removeStock(int stockId);
+    bool updateStock(int stockId, const QString &symbol, const QString &name, double price);
+    QVariantList getStock(int stockId);
+
+private:
+    QSqlDatabase db;
+};
