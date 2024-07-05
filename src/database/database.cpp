55ae9866031560aaa3d12ef4f1fcc9a0c7c98345
gufranky <g2676536169@gmail.com>
2024-07-05 09:32:52 +0800

    modified:   src/database/database.cpp 	modified:   src/database/database.h

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,3 +1,4 @@
+/*
 #include "database.h"
 void databaseInitialize()
 {
@@ -21,3 +22,4 @@ void databaseInitialize()
 
     }
 }
+*/
--- src/database/database.h
+++ src/database/database.h
@@ -7,6 +7,7 @@
 #include <QString>
 #include <QVariantList>
 #include <QObject>
+/*
 QSqlDatabase db;//database
 class Database : public QObject
 {
@@ -32,4 +33,4 @@ public:
 
 private:
     QSqlDatabase db;
-};
+};*/
