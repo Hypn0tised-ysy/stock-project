a6cc03683df08ca0711c28025462fc862667c593
gufranky <g2676536169@gmail.com>
2024-07-05 10:17:57 +0800

    python环境部署

--- StockMarket.pro
+++ StockMarket.pro
@@ -21,6 +21,8 @@ HEADERS += \
 
 FORMS += \
     $$files($$PWD/src/AllWindow/*.ui)
+INCLUDEPATH += -I $$PWD/env/include
+LIBS += -L $$PWD/env/libs -lpython39
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
+#include <python.h>
 #include "AllWindow/StockMarket.h"
-
 #include <QApplication>
 #pragma comment(lib, "user32.lib")
 
 int main(int argc, char *argv[])
 {
+    Py_SetPythonHome((wchar_t *)(L"./env"));
+    Py_Initialize();
+    if (!Py_IsInitialized())
+    {
+        return -1;
+    }
     QApplication a(argc, argv);
     StockMarket w;
     w.show();
     return a.exec();
+    // 以下为测试代码
 }
