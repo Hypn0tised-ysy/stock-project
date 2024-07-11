0bcf651210524898caec48a71a79606eb3adebe2
ysy <1481435885@qq.com>
2024-07-11 11:37:23 +0800

    eigen库

---
+++ src/database/predictor.cpp
@@ -0,0 +1,3 @@
+#include "predictor.h"
+
+predictor::predictor() {}
---
+++ src/database/predictor.h
@@ -0,0 +1,10 @@
+#ifndef PREDICTOR_H
+#define PREDICTOR_H
+
+class predictor
+{
+public:
+    predictor();
+};
+
+#endif // PREDICTOR_H
