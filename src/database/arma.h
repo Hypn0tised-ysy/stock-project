76403d367bfdcb4d6b2e10790dcb41be3603a0a2
Shunyu Ye <1481435885@qq.com>
2024-07-14 23:29:07 +0800

    fix bugs

--- src/database/arma.h
+++ src/database/arma.h
@@ -7,6 +7,7 @@
 
 class ARMA {
 public:
+    ARMA(){p=2;q=2;}
     ARMA(int p, int q);
     void fit(const std::vector<double>& data);//拟合 ARMA 模型。将时间序列数据保存到类的成员变量中，并调用 estimateParameters 函数来估计模型参数
     std::vector<double> predict(int steps);//steps对应预测的数据的个数
