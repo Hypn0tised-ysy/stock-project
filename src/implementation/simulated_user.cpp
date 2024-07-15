eb87784704c9bb822ed0d14ace5b25a094b03da7
zhaojl <1311769427@qq.com>
2024-07-15 11:06:36 +0800

    修改部分逻辑

--- src/implementation/simulated_user.cpp
+++ src/implementation/simulated_user.cpp
@@ -13,7 +13,7 @@ void Simulated_user::s_trade(double _price,int _quantity,QString &_symbol,bool _
 
 void create_s_acc() {
     for (int i = 0; i < 1000; i++) {
-        std::string name = "s_user" + std::to_string(i+1); // 生成用户id
+        std::string name = "s_user" + std::to_string(i+1); // ????id
         std::string password = "111111111";
 
         enroll(name, password);
@@ -22,59 +22,54 @@ void create_s_acc() {
 
 
 void simulated_trade() {
-    // 定义计时器周期为tick
-    const std::chrono::seconds timerInterval(30);
 
-    // 启动计时器
-    while (true) {
-        // 获取当前时间点
-        auto startTime = std::chrono::steady_clock::now();
-        //设置随机数种子
+        //???????
         srand(time(NULL));
-        //随机事件名称
+        //??????
         std::string ev = "Event";
-        //股价涨跌
+        //????
         bool gob = rand() % 2;
-        //发生事件e
+        //????e
         Event e(ev, gob);
-        //获取股票总数
+        //??????
         int n = db.getStocksList().size();
-        //股票编号
+        //????
         int s_id = rand() % n + 1;
-        //获取股票类型
+        //??????
         Stock s = db.getStocksList()[s_id - 1];
 
         e.impact(s);
         QString sym = QString::fromStdString(s.symbol);
-        // 模拟10个用户进行交易
+        // ??10???????
         for (int i = 0; i < 10; i++) {
-            // 随机生成交易订单数据
+            // ??????????
 
-            int peo_id = rand() % 1000 + 1;
+            int ra1 = rand() % db.getBotslist().size() + 1;
+            int peo_id = db.getBotslist()[ra1];
             double price;
             int quantity;
             double ra = (rand() % 5 + 1) / 100;
             Simulated_user robot(peo_id);
-            //胆大型用户（编号1到300）
-            if (1 <= peo_id && peo_id <= 300) {
-                //根据当前形势决定高价买入上涨股票或是低卖出下跌股票
-                //如果事件可能导致s股票上涨
+            //????????1?300?
+            if (ra1<300) {
+                //?????????????????????????
+                //????????s????
                 if (gob) {
-                    //高价买入大量股票
+                    //????????
                     price = s.market_price * (1 + ra);
                     quantity = 20 * (rand() % 9 + 1);
                     robot.s_trade(price, quantity, sym, !gob);
 
                 }
-                //如果事件不利于s股票
+                //???????s??
                 else {
-                    //检查仓库有无s股票，如果有，低价卖出
+                    //??????s???????????
                     for (int i = 0; i < db.getMyStock(peo_id).size(); i++)
                     {
                         if (s.symbol == db.getMyStock(peo_id)[i].get_name())
                         {
                             price = s.market_price * (1 - ra);
-                            //卖出一半s股票
+                            //????s??
                             quantity = db.getMyStock(peo_id)[i].get_sum() / 2;
                             robot.s_trade(price, quantity, sym, !gob);
                             break;
@@ -82,11 +77,11 @@ void simulated_trade() {
                     }
                 }
             }
-            //无脑型用户（编号301到700）
-            else if (300 < peo_id && peo_id <= 700)
+            //????????301?700?
+            else if (300 <= ra1 && ra1 < 700)
             {
                 bool ra2 = rand() % 2;
-                //高价买入s股票
+                //????s??
                 if (ra2)
                 {
 
@@ -95,7 +90,7 @@ void simulated_trade() {
                     robot.s_trade(price, quantity, sym, !gob);
                 }
                 else
-                {//如果仓库存在股票，低价售出
+                {//?????????????
                     for (int i = 0; i < db.getMyStock(peo_id).size(); i++)
                     {
                         if (s.symbol == db.getMyStock(peo_id)[i].get_name())
@@ -109,10 +104,10 @@ void simulated_trade() {
                 }
 
             }
-            //胆小型用户（编号701到1000）
-            else if (700 < peo_id <= 1000)
+            //????????701?1000?
+            else if (700 <= ra1&&ra1 < 1000)
             {
-                //如果事件有利于s股票，谨慎购入
+                //???????s???????
                 if (gob)
                 {
                     price = s.market_price * (1 + ra);
@@ -122,13 +117,13 @@ void simulated_trade() {
                 }
                 else
                 {
-                    //检查仓库有无s股票，如果有，低价卖出
+                    //??????s???????????
                     for (int i = 0; i < db.getMyStock(peo_id).size(); i++)
                     {
                         if (s.symbol == db.getMyStock(peo_id)[i].get_name())
                         {
                             price = s.market_price * (1 - ra);
-                            //卖出全部股票
+                            //??????
                             quantity = db.getMyStock(peo_id)[i].get_sum();
                             robot.s_trade(price, quantity, sym, !gob);
                             break;
@@ -137,15 +132,7 @@ void simulated_trade() {
                 }
             }
         }
-            // 计算下一个时间点
-            auto endTime = std::chrono::steady_clock::now();
-            auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime);
 
-            // 等待直到达到指定的时间间隔
-            if (elapsedTime < timerInterval) {
-                std::this_thread::sleep_for(timerInterval - elapsedTime);
-            }
-        }
 
 }
-}
+
